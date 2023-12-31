d74057ef78c222ecde0f448aabbd65ba985797f8
SauradipGhosh <sauradip96ghosh@gmail.com>
2024-01-01 00:25:58 +0530

    added tailwind css and create modularity

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
-import { DatePicker } from 'antd';
+import './stylesheets/alignments.css';
+import './stylesheets/custom-components.css';
+import './stylesheets/from-element.css';
+import './stylesheets/text-element.css';
+import './stylesheets/theme.css';
 import { Button } from 'antd';
 
 function App() {
   return (
     <div>
+      <div className={"bg-green-700"}>
+        hii
+      </div>
       <Button type='primary'>Primary Button</Button>
     </div>
   );
